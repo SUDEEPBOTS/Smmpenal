62c92712c52b45292bf2626e1baaa1e8fd40fbbb
𝐒𝐔𝐃𝐄𝐄𝐏𝐁𝐎𝐓𝐒 <sudeeptelegram@gmail.com>
2026-01-09 11:38:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,3 +16,6 @@ MONGO_URL = os.getenv("MONGO_URL", "")
 # Father SMM API
 SMM_API_URL = os.getenv("SMM_API_URL", "https://fathersmm.com/api/v2")
 SMM_API_KEY = os.getenv("SMM_API_KEY", "")
+
+# config.py mein ye line add kar de
+GROQ_API_KEY = os.getenv("GROQ_API_KEY", "gsk_TERI_KEY_YAHAN")
